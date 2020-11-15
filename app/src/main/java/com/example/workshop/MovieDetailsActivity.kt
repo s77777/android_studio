631c0b355e92cde9d6b69a6f55009a39788f9d35
Sergey Levchenko <926113@gmail.com>
2020-11-15 20:59:50 +0300

    ws3

---
+++ app/src/main/java/com/example/workshop/MovieDetailsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.workshop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MovieDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_details)
+    }
+}
