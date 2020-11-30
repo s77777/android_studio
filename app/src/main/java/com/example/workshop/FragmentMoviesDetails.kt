466a0e76d86cc372e3054fd02572c2bdc25a26fc
Sergey Levchenko <926113@gmail.com>
2020-11-30 13:03:14 +0300

    #movielist

--- app/src/main/java/com/example/workshop/FragmentMoviesDetails.kt
+++ app/src/main/java/com/example/workshop/FragmentMoviesDetails.kt
@@ -1,11 +1,16 @@
 package com.example.workshop
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class FragmentDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_movie_details)
+        findViewById<TextView>(R.id.back).setOnClickListener{
+            startActivity(Intent(this, MainActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/example/workshop/MainActivity.kt
+++ app/src/main/java/com/example/workshop/MainActivity.kt
@@ -4,10 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.appcompat.widget.AppCompatButton
+import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.fragment_movies_list)
+        findViewById<CardView>(R.id.card).setOnClickListener {
+                startActivity(Intent(this, FragmentDetailsActivity::class.java))
+        }
     }
 }
